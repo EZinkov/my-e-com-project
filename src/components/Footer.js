f5e40b8c69d31cbf5441188be24694731c3c1762
Evgeniy <evgeniyzinkov98@gmail.com>
2023-04-17 16:51:44 +0200

    feat: footer design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react"
 
 const Footer = () => {
-  return <div>Footer</div>;
-};
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <p className="text-white text-center">
+          Copyright &copy; Ecommerce Shop 2023. All rights reserverd.
+        </p>
+        <p className="text-white text-center">
+          <a href="https://www.linkedin.com/in/evgeniyzinkov/" target="_blank">
+            Contact Us
+          </a>
+        </p>
+      </div>
+    </footer>
+  )
+}
 
-export default Footer;
+export default Footer
