b7ba70010f1c02943097819a7cc0f90ad79c1bc7
Evgeniy <evgeniyzinkov98@gmail.com>
2023-04-17 16:52:05 +0200

    feat: navbar & sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,45 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { BsBag } from "react-icons/bs"
 
 import { SidebarContext } from "../contexts/SidebarContext"
+import { CartContext } from "../contexts/CartContext"
+import { Link } from "react-router-dom"
+
+import Logo from "../img/logo.svg"
+import { useEffect } from "react"
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false)
   const { isOpen, setIsOpen } = useContext(SidebarContext)
+  const { itemAmount } = useContext(CartContext)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false)
+    })
+  })
 
   return (
-    <header className="bg-pink-200">
-      <div>Header</div>
-      <div
-        onClick={() => setIsOpen(!isOpen)}
-        className="cursor-pointer flex relative"
-      >
-        <BsBag className="text-2xl" />
+    <header
+      className={`${
+        isActive ? "bg-white py-4 shadow-md" : "bg-none py-6"
+      } fixed w-full z-10 transition-all`}
+    >
+      <div className="flex container mx-auto items-center justify-between h-full">
+        <Link to={"/"}>
+          <div>
+            <img className="w-[40px]" src={Logo} />
+          </div>
+        </Link>
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          className="cursor-pointer flex relative"
+        >
+          <BsBag className="text-2xl" />
+          <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center">
+            {itemAmount}
+          </div>
+        </div>
       </div>
     </header>
   )
